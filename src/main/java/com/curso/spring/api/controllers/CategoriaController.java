4198f2f70c3ec3fd98894302157c50770af34d44
Alex <alex.novoa.r@gmail.com>
2020-06-13 11:24:22 -0500

    Se aumentaron los metodos PUT y DELETE

--- src/main/java/com/curso/spring/api/controllers/CategoriaController.java
+++ src/main/java/com/curso/spring/api/controllers/CategoriaController.java
@@ -9,9 +9,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,12 +26,7 @@ public class CategoriaController {
     @Autowired
     private BCryptPasswordEncoder bcrypt;
 
-    @GetMapping("/saludar/{nombre}")
-    public String saludar(@PathVariable String nombre) {
-        return "HOLA "+nombre;
-    }
-
-
+   
     @GetMapping("/categorias/{id}")   
     public ResponseEntity<Response<Categoria>> getCategoriaPorId(@PathVariable int id){
         Response<Categoria> response = new Response<>();  
@@ -55,5 +52,34 @@ public class CategoriaController {
     public Categoria postCategoria(@RequestBody Categoria categoria ){
         return categoriaRepository.save(categoria);   
     } 
+
+    @DeleteMapping("/categorias/{id}")   
+    public ResponseEntity<Response<Categoria>> delete(@PathVariable int id){
+        Response<Categoria> response = new Response<>();  
+        Categoria categoria = categoriaRepository.findById(id);     
+        if(categoria==null){
+            response.getErrores().add("No existe el elemento");           
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+        }
+        categoriaRepository.delete(categoria);
+        response.setResultado(categoria);       
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/categorias/{id}")   
+    public ResponseEntity<Response<Categoria>> put(@PathVariable int id, @RequestBody Categoria categoria){
+        Response<Categoria> response = new Response<>();  
+        Categoria categoriaExistente = categoriaRepository.findById(id);     
+        if(categoria==null){
+            response.getErrores().add("No existe el elemento");           
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+        }
+        categoriaExistente.setDescripcion(categoria.getDescripcion());
+        categoriaRepository.save(categoriaExistente);
+        response.setResultado(categoriaExistente);       
+        return ResponseEntity.ok(response);
+    }
+
+  
     
 }
